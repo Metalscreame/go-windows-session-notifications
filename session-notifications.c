ff3aa7ff738d86a265308dfec96bd65fecca60af
RomanKosyii <rkosy@continuum.net>
2019-04-05 11:44:23 +0300

    switched to ctrl handler to catch events

--- session-notifications.c
+++ session-notifications.c
@@ -2,60 +2,33 @@
 #include <WtsApi32.h>
 #include "_cgo_export.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+#define CTRL_SHUTDOWN  10
+#define CTRL_LOGOFF    20
+#define CTRL_ERROR     999
+
+BOOL WINAPI HandlerRoutine(DWORD dwCtrlType)
 {
-    int wmId, wmEvent;
-    switch (message)
-    {
-    case WM_QUERYENDSESSION:
-        relayMessage(message, lParam);
-        break;
-    case WM_WTSSESSION_CHANGE:
-        relayMessage(message, wParam);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-DWORD WINAPI WatchSessionNotifications(LPVOID lpParam)
-{
-    WNDCLASS wc;
-    HWND hwnd;
-    MSG msg;
-
-    char const *lpClassName="classWatchSessionNotifications";
-
-    wc.lpfnWndProc = WndProc;
-    wc.lpszClassName=lpClassName;
-
-    if (!RegisterClass(&wc))
-        return 0;
-
-    hwnd = CreateWindow(lpClassName,
-                        lpClassName,
-                        WS_OVERLAPPEDWINDOW,
-                        CW_USEDEFAULT,CW_USEDEFAULT,100,100,
-                        NULL,NULL,NULL,NULL);
-
-    if (!hwnd)
-        return 0;
-
-    UpdateWindow(hwnd);
-    WTSRegisterSessionNotification(hwnd, NOTIFY_FOR_THIS_SESSION);
-
-    while (GetMessage(&msg,NULL,0,0) > 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+	switch (dwCtrlType)
+	{
+	case CTRL_SHUTDOWN_EVENT:
+		{
+            relayMessage(CTRL_SHUTDOWN, CTRL_SHUTDOWN);
+			return TRUE;
+		}
+	case CTRL_LOGOFF_EVENT:
+		{
+        relayMessage(CTRL_LOGOFF, CTRL_LOGOFF);
+			return TRUE;
+		}
+	default:
+		{
+        relayMessage(CTRL_ERROR, CTRL_ERROR);
+		}
+	}
+	return FALSE;
 }
 
 void Stop(HANDLE hndl) {
-    // TODO: Figure out how to ask the thread to quit by itself
-    // PostThreadMessage(hndl, WM_QUIT, 0, 0);
-    // WaitForSingleObject(hndl, 5000);
     TerminateThread(hndl, 0);
 }
 
@@ -63,16 +36,8 @@ HANDLE Start() {
     DWORD lpThreadId, lpParameter = 1;
     HANDLE hThread;
 
-    hThread = CreateThread(
-                  NULL,
-                  0,
-                  WatchSessionNotifications,
-                  &lpParameter,
-                  0,
-                  &lpThreadId);
+    if (!SetConsoleCtrlHandler(HandlerRoutine, TRUE))
+                relayMessage(CTRL_ERROR, CTRL_ERROR);
 
-    if (hThread == NULL)
-        return 0;
-    else
-        return hThread;
+    while (1){}
 }
